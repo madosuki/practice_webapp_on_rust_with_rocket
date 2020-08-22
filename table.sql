b866d830fe0c9bc1acd97876e2340a0deb28d8c7
madosuki <yurikasamaaikatsu@gmail.com>
2020-08-22 15:15:54 +0900

    add table.sql

---
+++ table.sql
@@ -0,0 +1,8 @@
+create table students(id integer primary key,
+       name text not null,
+       year integer not null,
+       class_name text not null,
+       age integer not null,
+       is_exist bool not null default false)
+
+
